f1cc540002393b76de789b2d758573757947c42a
João Vítor Correia Pessoa <joaovitorpessoa10@gmail.com>
2025-10-10 17:44:17 -0300

    Add docs

--- scripts/cpu/plot_ber_compare.m
+++ scripts/cpu/plot_ber_compare.m
@@ -14,7 +14,7 @@ env_file = fullfile(current_dir, '..', '..', '.env');
 env_vars = load_env(env_file);
 
 % simulation_dir = env_vars.GPU_SIMULATION_SAVE_PATH;
-simulation_dir = env_vars.SIMULATION_PATH_16QAM;
+simulation_dir = env_vars.SIMULATION_PATH_64QAM;
 plot_dir = env_vars.GPU_PLOT_BER_PATH;
 functions_dir = env_vars.CPU_FUNCTIONS_PATH;
 
@@ -45,8 +45,8 @@ colors = [0.0000 0.0000 0.0000;
 % ####################################################################### %
 
 x = 'dl';
-y = 'zf';
-z = '16qam';
+y = 'mmse';
+z = '64qam';
 
 filename = sprintf('%s_ber_%s_ss_256_64.mat', x, y);
 load(filename);
@@ -107,6 +107,7 @@ set(gca, 'FontName', fontname, 'FontSize', fontsize);
 ylim([1e-3 1])
 
 graph_name = sprintf('%s_ber_compare_%s_%s', x, y, z);
+
 
 if savefig == 1
     saveas(gcf, fullfile(plot_dir, [graph_name '.eps']), 'epsc2');
--- scripts/cpu/plot_constellation.m
+++ scripts/cpu/plot_constellation.m
@@ -52,9 +52,9 @@ colors = [0.0000 0.0000 0.0000;  % Preto
 %% PLOT
 % ####################################################################### %
 
-e = 'dl';
-precoder_type = 'MMSE';
-z = '16qam';
+e = 'ul';
+decoder_type = 'MMSE';
+z = '256qam';
 
 amplifiers_type = {'IDEAL', 'SS'};
 N_AMP = length(amplifiers_type);
@@ -64,10 +64,10 @@ K = 64;
 K_f = 10;
 
 N_BLK = 250;
-A0 = [0.5, 1.0, 1.5 ,2.0];
+A0 = [0.5, 1.0, 2.0];
 N_A0 = length(A0);
 
-B = 4;
+B = 8;
 M_QAM = 2^B;
 
 SNR = 30;
@@ -97,63 +97,61 @@ H = H_LOS + sqrt(1 / (1 + K_f)) * sqrtm(R) * H_NLOS;
 %% Uplink
 % ####################################################################### %
 
-% bit_array = randi([0, 1], B * N_BLK, K);
-% s  = (qammod(bit_array, M_QAM,'InputType','bit')).'; 
-% Ps = vecnorm(s,2,2).^2/N_BLK;
-% 
-% s_normalized = s./sqrt(Ps);
-% 
-% v = sqrt(0.5) * (randn(M, N_BLK) + 1i*randn(M, N_BLK));
-% Pv = vecnorm(v,2,2).^2 / N_BLK;
-% v_normalized = v./sqrt(Pv);
-% 
-% for a_idx = 1:N_A0
-%     for amp_idx = 1:N_AMP
-% 
-%         a0 = A0(a_idx);
-%         current_amp_type = amplifiers_type{amp_idx};
-% 
-%         y = H * sqrt(snr) * amplify_signal(s_normalized, current_amp_type, a0) + v_normalized;
-% 
-%         decoder = decode_signal(decoder_type, H, snr);
-%         s_hat = decoder * y;
-%         Ps_hat = vecnorm(s_hat,2,2).^2/N_BLK;
-%         s_hat_normalized(:,:,amp_idx,a_idx) = s_hat.*sqrt(Ps./Ps_hat);
-%     end
-% end
-
-% ####################################################################### %
-%% Downlink
-% ####################################################################### %
-
-b = randi([0 1], N_BLK*B,K);
-s  = (qammod(b, M_QAM,'InputType','bit')).';
+bit_array = randi([0, 1], B * N_BLK, K);
+s  = (qammod(bit_array, M_QAM,'InputType','bit')).'; 
 Ps = vecnorm(s,2,2).^2/N_BLK;
-s_normalized = s./sqrt(Ps);
-eta = 1/K;
-    
-precoder = sqrt(M-K) * (conj(H) / (H.' * conj(H)));
-x = precoder*sqrt(eta)*s_normalized;
-%x = precoder(:,:,snr_idx)*sqrt(eta)*s_norm;
 
-v = sqrt(0.5)*(randn(K,N_BLK) + 1i*randn(K,N_BLK));
-Pv = vecnorm(v,2,2).^2/N_BLK;
-v_norm = v./sqrt(Pv);
-      
+s_normalized = s./sqrt(Ps);
+
+v = sqrt(0.5) * (randn(M, N_BLK) + 1i*randn(M, N_BLK));
+Pv = vecnorm(v,2,2).^2 / N_BLK;
+v_normalized = v./sqrt(Pv);
+
 for a_idx = 1:N_A0
     for amp_idx = 1:N_AMP
+
         a0 = A0(a_idx);
         current_amp_type = amplifiers_type{amp_idx};
-        
-        y = H.' * sqrt(snr) * amplify_signal(x, current_amp_type, a0) + v_norm;
-        
-        s_hat = y;
+
+        y = H * sqrt(snr) * amplify_signal(s_normalized, current_amp_type, a0) + v_normalized;
+
+        decoder = decode_signal(decoder_type, H, snr);
+        s_hat = decoder * y;
         Ps_hat = vecnorm(s_hat,2,2).^2/N_BLK;
         s_hat_normalized(:,:,amp_idx,a_idx) = s_hat.*sqrt(Ps./Ps_hat);
     end
 end
 
-
+% ####################################################################### %
+%% Downlink
+% ####################################################################### %
+% 
+% b = randi([0 1], N_BLK*B,K);
+% s  = (qammod(b, M_QAM,'InputType','bit')).';
+% Ps = vecnorm(s,2,2).^2/N_BLK;
+% s_normalized = s./sqrt(Ps);
+% eta = 1/K;
+% 
+% precoder = sqrt(M-K) * (conj(H) / (H.' * conj(H)));
+% x = precoder*sqrt(eta)*s_normalized;
+% %x = precoder(:,:,snr_idx)*sqrt(eta)*s_norm;
+% 
+% v = sqrt(0.5)*(randn(K,N_BLK) + 1i*randn(K,N_BLK));
+% Pv = vecnorm(v,2,2).^2/N_BLK;
+% v_norm = v./sqrt(Pv);
+% 
+% for a_idx = 1:N_A0
+%     for amp_idx = 1:N_AMP
+%         a0 = A0(a_idx);
+%         current_amp_type = amplifiers_type{amp_idx};
+% 
+%         y = H.' * sqrt(snr) * amplify_signal(x, current_amp_type, a0) + v_norm;
+% 
+%         s_hat = y;
+%         Ps_hat = vecnorm(s_hat,2,2).^2/N_BLK;
+%         s_hat_normalized(:,:,amp_idx,a_idx) = s_hat.*sqrt(Ps./Ps_hat);
+%     end
+% end
 
 % ####################################################################### %
 %% PLOTAGEM DA CONSTELAÇÃO
@@ -162,7 +160,7 @@ end
 figure;
 set (gcf, "Position", [0, 0, 800, 600]);
 
-subplot(2,2,1)
+%subplot(2,2,1)
 plot(real(s_hat_normalized(:,:,1,1)), imag(s_hat_normalized(:,:,1,1)),'.','MarkerSize', markersize,'Color',colors(2, :));
 hold on;
 plot(real(s_hat_normalized(:,:,2,1)), imag(s_hat_normalized(:,:,2,1)),'.','MarkerSize', markersize,'Color',colors(3, :));
@@ -171,7 +169,14 @@ ylabel('Q', 'FontName', fontname, 'FontSize', fontsize);
 set(gca, 'FontName', fontname, 'FontSize', fontsize);
 hold off;
 
-subplot(2,2,2)
+graph_name = sprintf('constellation_%s_%s_%d_%d_%s_05a', e, lower(decoder_type), M, K, z);
+
+if savefig == 1
+    saveas(gcf,[plot_dir graph_name],'epsc2');
+    saveas(gcf,[plot_dir graph_name],'png');
+end
+
+%subplot(2,2,2)
 plot(real(s_hat_normalized(:,:,1,1)), imag(s_hat_normalized(:,:,1,1)),'.','MarkerSize', markersize,'Color',colors(2, :));
 hold on;
 plot(real(s_hat_normalized(:,:,2,2)), imag(s_hat_normalized(:,:,2,2)),'.','MarkerSize', markersize,'Color',colors(4, :));
@@ -180,7 +185,14 @@ ylabel('Q', 'FontName', fontname, 'FontSize', fontsize);
 set(gca, 'FontName', fontname, 'FontSize', fontsize);
 hold off;
 
-subplot(2,2,3)
+graph_name = sprintf('constellation_%s_%s_%d_%d_%s_10a', e, lower(decoder_type), M, K, z);
+
+if savefig == 1
+    saveas(gcf,[plot_dir graph_name],'epsc2');
+    saveas(gcf,[plot_dir graph_name],'png');
+end
+
+%subplot(2,2,3)
 plot(real(s_hat_normalized(:,:,1,1)), imag(s_hat_normalized(:,:,1,1)),'.','MarkerSize', markersize,'Color',colors(2, :));
 hold on;
 plot(real(s_hat_normalized(:,:,2,3)), imag(s_hat_normalized(:,:,2,3)),'.','MarkerSize', markersize,'Color',colors(5, :));
@@ -189,32 +201,9 @@ ylabel('Q', 'FontName', fontname, 'FontSize', fontsize);
 set(gca, 'FontName', fontname, 'FontSize', fontsize);
 hold off;
 
-subplot(2,2,4)
-plot(real(s_hat_normalized(:,:,1,1)), imag(s_hat_normalized(:,:,1,1)),'.','MarkerSize', markersize,'Color',colors(2, :));
-hold on;
-plot(real(s_hat_normalized(:,:,2,4)), imag(s_hat_normalized(:,:,2,4)),'.','MarkerSize', markersize,'Color',colors(6, :));
-xlabel('I', 'FontName', fontname, 'FontSize', fontsize);
-ylabel('Q', 'FontName', fontname, 'FontSize', fontsize);
-set(gca, 'FontName', fontname, 'FontSize', fontsize);
-hold off;
-
-graph_name = sprintf('constellation_%s_%s_%d_%d_%s', e, lower(precoder_type), M, K, z);
-graph_name
+graph_name = sprintf('constellation_%s_%s_%d_%d_%s_20a', e, lower(decoder_type), M, K, z);
 
 if savefig == 1
     saveas(gcf,[plot_dir graph_name],'epsc2');
     saveas(gcf,[plot_dir graph_name],'png');
 end
-
-% uma pergunta para fazer e ver
-% variar constelação ou precoder/decoder - Testar para ver
-% Figura 1 - 16 - QAM Ideal e SSA (A = 0.5)
-% Figura 2 - 16 - QAM Ideal e SSA (A = 1.0)
-% Figura 3 - 16 - QAM Ideal e SSA (A = 2.0)
-% Figura 4 - 64 - QAM Ideal e SSA (A = 0.5)
-% Figura 5 - 64 - QAM Ideal e SSA (A = 1.0)
-% Figura 6 - 64 - QAM Ideal e SSA (A = 2.0)
-% -- SE FICAR BOM MOSTRA
-% Figura 7 - 256 - QAM Ideal e SSA (A = 0.5)
-% Figura 8 - 256 - QAM Ideal e SSA (A = 1.0)
-% Figura 9 - 256 - QAM Ideal e SSA (A = 2.0)
